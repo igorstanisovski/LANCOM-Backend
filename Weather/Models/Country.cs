ce0731a9585d2d0a8dae749cc56e54e172e3b6df
Igor Stanishovski <igor.stanisovski@gmail.com>
2020-11-05 10:40:47 +0100

    feature/country-class

---
+++ Weather/Models/Country.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Weather.Models
+{
+    public class Country
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "{0} cannot be longer than 50 characters.")]
+        public string Name { get; set;  }
+    }
+}
